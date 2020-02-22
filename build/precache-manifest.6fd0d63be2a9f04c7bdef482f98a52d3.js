e8153ee4a061a98b42a84251f799d85bcb5cfc3b
Amanat <amnaatmurzaliev@gmail.com>
2020-02-21 17:19:55 -0800

    First deploy

---
+++ build/precache-manifest.6fd0d63be2a9f04c7bdef482f98a52d3.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc7859be83451b050c3a23634f333a45",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a598a8b4c3b598388f1",
+    "url": "/static/js/2.8eb126e9.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.8eb126e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4beed5479719b577800b",
+    "url": "/static/js/main.f4b737c0.chunk.js"
+  },
+  {
+    "revision": "df789f841de742fc7ba6",
+    "url": "/static/js/runtime-main.830788a2.js"
+  },
+  {
+    "revision": "8f4f2217bc7e438c077364218b157c77",
+    "url": "/static/media/contractors.8f4f2217.svg"
+  },
+  {
+    "revision": "c454b805422e9b46059f74298cc6367d",
+    "url": "/static/media/dealer_centers.c454b805.svg"
+  },
+  {
+    "revision": "b4227c1e2872a64f7e8d5cb849db9267",
+    "url": "/static/media/dispatchers.b4227c1e.svg"
+  },
+  {
+    "revision": "5f44fc68478906e4af72424e5b2994dc",
+    "url": "/static/media/history.5f44fc68.svg"
+  },
+  {
+    "revision": "603d5388c65d40ef1df71ba8eb6993a9",
+    "url": "/static/media/logo.603d5388.svg"
+  },
+  {
+    "revision": "9d6e7536d2008b0b017458896ac7a958",
+    "url": "/static/media/logout.9d6e7536.svg"
+  },
+  {
+    "revision": "197c485d19b2f4638f6a9243a861a208",
+    "url": "/static/media/maps.197c485d.svg"
+  },
+  {
+    "revision": "3c2ea25c7dd0aec4d0e92e9553084336",
+    "url": "/static/media/orders.3c2ea25c.svg"
+  },
+  {
+    "revision": "473a3c997fb2020932598b312bb490c9",
+    "url": "/static/media/reports.473a3c99.svg"
+  },
+  {
+    "revision": "d67a5f72a6378bec90d2d4789e2cfe4e",
+    "url": "/static/media/reviews.d67a5f72.svg"
+  },
+  {
+    "revision": "5f699bc7da54c11e4fc37bcafcf87d3c",
+    "url": "/static/media/settings.5f699bc7.svg"
+  }
+]);
